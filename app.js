e70ebc334e13b61ca819d15f5de725ff79bb1665
abdelrahman <gadoua50@gmail.com>
2022-05-25 20:01:44 +0200

    add code to connect to MongoDB

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Set up mongoose connection
+const mongoDB =
+  "mongodb+srv://abdo:abdo@cluster0.ezsi8.mongodb.net/local_library?retryWrites=true&w=majority";
+
+mongoose.connect(monogoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
