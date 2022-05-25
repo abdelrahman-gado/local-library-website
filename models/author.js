27a8e064eddbe368bfd4d543cef531a4bc0278bc
abdelrahman <gadoua50@gmail.com>
2022-05-25 20:02:43 +0200

    define the LocalLibrary Schemas and Models

---
+++ models/author.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+// Virtual for author's full name
+AuthorSchema.virtual("name").get(() => {
+  // To avoid errors in cases where an author does not have a first name or a family name
+  // We will handle these cases by returning an empty string
+  let fullname = "";
+  if (this.first_name && this.family_name) {
+    fullname = this.first_name + ", " + this.family_name;
+  }
+
+  if (!this.first_name || !this.family_name) {
+    fullname = "";
+  }
+
+  return fullname;
+});
+
+// Virtual for author lifespan
+AuthorSchema.virtual("lifespan").get(() => {
+  let lifetime_string = "";
+  if (this.date_of_birth) {
+    lifetime_string = this.date_of_birth.getYear().toString();
+  }
+
+  lifetime_string += " - ";
+
+  if (this.date_of_death) {
+    lifetime_string += this.date_of_death.getYear();
+  }
+
+  return lifetime_string;
+});
+
+
+// Virtual for author's URL
+AuthorSchema.virtual('url').get(() => {
+  return '/catalog/author/' + this._id;
+});
+
+
+// Export Model
+module.exports = mongoose.model('Author', AuthorSchema);
