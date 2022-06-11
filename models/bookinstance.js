6f280ae01fda430ac4e89251691c4826af76294d
abdelrahman <gadoua50@gmail.com>
2022-06-11 12:58:36 +0200

    fix virtual url property to be /bookinstance instead of /catalog/bookinstance

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -17,7 +17,7 @@ const BookInstanceSchema = new Schema({
 
 // Virtual for bookInstance's URL
 BookInstanceSchema.virtual("url").get(function () {
-  return "catalog/bookinstace/" + this._id;
+  return "bookinstance/" + this._id;
 });
 
 BookInstanceSchema.virtual("due_back_formatted").get(function () {
